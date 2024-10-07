912db042ab3d2c238a3a28a163ab1d6ce17c0781
donghoon99 <donghoon900@gmail.com>
2024-10-07 11:21:33 +0900

    new file:   ggd/package-lock.json 	modified:   ggd/package.json 	modified:   ggd/src/components/ShoppingMall.jsx 	modified:   ggd/yarn.lock

--- ggd/src/components/ShoppingMall.jsx
+++ ggd/src/components/ShoppingMall.jsx
@@ -5,6 +5,23 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const ShoppingMall = () => {
+  function Slider() {
+    const [slideIndex, setSlideIndex] = useState(1);
+    return (
+      <Container>
+        <Arrow direction="prev">이전</Arrow>
+        {data.map((character) => (
+          <Slide
+          key={character.id}
+          className={character.id === slideIndex ? "active" : null}
+          >
+            <Photo
+            ></Photo>
+          </Slide>
+        ))}
+      </Container>
+    )
+  }
   return (
     <div>
       <Header />
