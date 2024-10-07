4dd0d7d63c1428835762b288a357bcbccc17f713
jong-whd <jongwhd03@gmail.com>
2024-10-07 10:52:17 +0900

    modified:   ggd/src/components/header.jsx 	modified:   ggd/src/components/Header.jsx

--- ggd/src/components/header.jsx
+++ ggd/src/components/header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
-  return <div>헤더</div>;
+  return <div>
+    <Link to="/main"></Link> 
+
+  </div>;
 };
 
 export default Header;
