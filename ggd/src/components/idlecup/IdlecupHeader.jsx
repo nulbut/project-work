a946f6f191e3c2264b7bf7f7da1529e6f4af75ae
nulbut <kkr00629@gmail.com>
2024-10-07 17:13:20 +0900

    modified:   ggd/src/components/idlecup/IdlecupHeader.jsx 	modified:   ggd/src/components/idlecup/IdlecupSidebar.jsx 	new file:   ggd/src/components/idlecup/scss/IdlecupHeader.scss 	modified:   ggd/src/components/idlecup/scss/IdlecupSidebar.scss

--- ggd/src/components/idlecup/IdlecupHeader.jsx
+++ ggd/src/components/idlecup/IdlecupHeader.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import logo from "../images/logo.svg";
+import "./scss/IdlecupHeader.scss";
 
 const IdlecupHeader = () => {
   return (
     <header>
-      이상형 월드컵 <img src={logo} />
+      <div>
+        이상형 월드컵
+        <img src={logo} />
+      </div>
     </header>
   );
 };
