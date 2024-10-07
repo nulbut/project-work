dd8c1d6544a9c61391c814bab7086db8fd070d5f
donghoon99 <donghoon900@gmail.com>
2024-10-07 10:03:33 +0900

    project-work

--- ggd/src/components/test01.jsx
+++ ggd/src/components/test01.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import "./scss/Header.scss";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fontawesome/free-solid-svg-icons";
 
-const test01 = () => {
+//24-10-07 09:30 시작
+const test01 = ({ lstate, onLogout }) => {
+    const { logid, mlink } = lstate;
+    const homeLink = logid === "" ? "/" : "/main";
+
     return (
         <div>
             
