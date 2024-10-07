937bf2f0eb8b0e747be142abbfc7af35d92c5508
nulbut <kkr00629@gmail.com>
2024-10-07 11:18:57 +0900

    modified:   ggd/src/App.jsx 	new file:   ggd/src/components/Hot.jsx 	modified:   ggd/src/components/Idlecup.jsx 	new file:   ggd/src/components/ShopLayout.jsx 	modified:   ggd/src/components/ShoppingMall.jsx 	new file:   ggd/src/components/Used.jsx

---
+++ ggd/src/components/Hot.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hot = () => {
+  return <div>인기상품</div>;
+};
+
+export default Hot;
--- ggd/src/components/Idlecup.jsx
+++ ggd/src/components/Idlecup.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useCallback, useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
 
 const Idlecup = () => {
   return <div>이상형월드컵</div>;
