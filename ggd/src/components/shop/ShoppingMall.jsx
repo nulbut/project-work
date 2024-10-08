4579ea22331a5527d6eb9bf2ede888c8a20ad1e5
jong-whd <jongwhd03@gmail.com>
2024-10-08 14:24:11 +0900

    수정

--- ggd/src/components/shop/ShoppingMall.jsx
+++ ggd/src/components/shop/ShoppingMall.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-
+import Slider from "./Slider";
 import { Link, Outlet } from "react-router-dom";
 
 const ShoppingMall = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/new">최신</Link>
-        </li>
-        <li>
-          <Link to="/used">중고</Link>
-        </li>
-        <li>
-          <Link to="/hot">인기</Link>
-        </li>
-      </ul>
+      <Slider />
     </div>
   );
 };
