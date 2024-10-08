da5a2f47c8bd54213d77f9e6a325e8a9c9d15315
donghoon99 <donghoon900@gmail.com>
2024-10-08 13:43:43 +0900

    project-work

--- ggd/src/components/shop/ShoppingMall.jsx
+++ ggd/src/components/shop/ShoppingMall.jsx
@@ -5,17 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 const ShoppingMall = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/new">최신</Link>
-        </li>
-        <li>
-          <Link to="/used">중고</Link>
-        </li>
-        <li>
-          <Link to="/hot">인기</Link>
-        </li>
-      </ul>
+      
     </div>
   );
 };
