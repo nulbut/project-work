a63a2fd7922c9f46db9a04f74f344b68ae241b18
donghoon99 <donghoon900@gmail.com>
2024-10-08 16:34:29 +0900

    project-work

--- ggd/src/components/shop/ShoppingMall.jsx
+++ ggd/src/components/shop/ShoppingMall.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Slideshow from "./Slideshow";
 
 import { Link, Outlet } from "react-router-dom";
 
 const ShoppingMall = () => {
   return (
     <div>
-      
+      <Slideshow />
     </div>
   );
 };
