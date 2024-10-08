e257b8d2630abe27defbb133b9e349ebbc842b8c
your_name <your_email>
2024-10-08 16:58:02 +0900

    modified:   ggdserver/src/main/java/com/icia/ggdserver/controller/NMemberController.java 	modified:   ggdserver/src/main/java/com/icia/ggdserver/repository/NMemberRepository.java 	modified:   ggdserver/src/main/java/com/icia/ggdserver/service/NMemberService.java

--- ggdserver/src/main/java/com/icia/ggdserver/controller/NMemberController.java
+++ ggdserver/src/main/java/com/icia/ggdserver/controller/NMemberController.java
@@ -1,10 +1,27 @@
 package com.icia.ggdserver.controller;
 
+import com.icia.ggdserver.entity.nmemberTbl;
+import com.icia.ggdserver.service.NMemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
-public class MemberController {
+public class NMemberController {
+    @Autowired
+    private NMemberService nmServ;
 
+    @PostMapping("nidCheck")
+    public Map<String,String> nidCheck(@RequestBody nmemberTbl nmemberTbl){
+        log.info("nidCheck() mid : {}", nmemberTbl.getN_id());
+
+        Map<String, String> rsMap = nmServ.nidCheck(nmemberTbl.getN_id());
+
+        return rsMap;
+    }
 }
