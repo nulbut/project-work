fb7b92f33eee64d57f8623b2f818bd9768a414c7
your_name <your_email>
2024-10-08 09:18:11 +0900

    modified:   ggd/src/components/shop/JoinN.jsx

--- ggd/src/components/shop/JoinN.jsx
+++ ggd/src/components/shop/JoinN.jsx
@@ -6,48 +6,53 @@ const JoinN = () => {
         <div className='join'>
             <form className="Content">
                <div className='id'>
-                <div>ID</div>
-                <button>
+                <p>ID
+                    <button>
                     ID 중복 확인
                 </button>
+                </p>
                 <input placeholder='영어/대소문자 4~12자' />
                </div>
                <div className='nickname'>
-               <div>닉네임</div>
-               <button>
+               <p>닉네임 
+                <button>
                 닉네임 중복 확인
                </button>
+               </p>
                <input placeholder='영어/대소문자 4~12자' />
                </div>
                <div className='password'>
-               <div>Password</div>
+               <p>Password</p>
                <input placeholder=
                '영문,숫자,특수문자 중 2종류 이상을 조합하여 최소 8자리' />
-               <div>비밀번호 확인</div>
+               <p>비밀번호 확인</p>
                <input placeholder=
                '영문,숫자,특수문자 중 2종류 이상을 조합하여 최소 8자리' />
                </div>
                <div className='name'>
-                <div>이름</div>
+                <p>이름</p>
                 <input placeholder='이름' />
                </div>
                <div className='gender'>
-                <div>성별</div>
+                <p>성별</p>
                 <input type='radio' />남성
                 <input type='radio' />여성
                </div>
                <div className='birthday'>
-                <div>생년월일</div>
+                <p>생년월일</p>
                 <input placeholder='YYYY-MM-DD' />
                </div>
                <div className='phonenum'>
-                <div>핸드폰 번호</div>
-                <select>
+                <p>핸드폰 번호</p>
+                <p><select>
                     <option value="1">010</option>
                     <option value="2">02</option>
                     <option value="3">032</option>
                     <option value="4">070</option>
                 </select>
+                -<input />
+                -<input />
+                </p>
                </div>
             </form>
         </div>
