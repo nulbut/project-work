1ddff6bdd7347fd8731ccd28b6be99d5410bf9c9
donghoon99 <donghoon900@gmail.com>
2024-10-16 09:45:00 +0900

    project-work

--- ggd/src/components/shop/InfiniteScroll.jsx
+++ ggd/src/components/shop/InfiniteScroll.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./scss/InfiniteScroll.scss";
 
-// 24-10-10 09:10 시작
+// 24-10-16 09:20 더미이미지 -> 이미지 넣고 다시해보기
 function InfiniteScroll() {
   const [products, setProducts] = useState([]);
   const [hasMore, setHasMore] = useState(true);
@@ -51,7 +51,7 @@ function InfiniteScroll() {
         {products.map((product, index) => (
           <div key={index} className="product-card">
             <img
-              src="./images/27.JPG"
+              // src="./images/27.JPG"
               alt={`상품 이미지 ${index + 1}`}
               className="product-image"
             />
@@ -74,7 +74,7 @@ function InfiniteScroll() {
         {products.map((product, index) => (
           <div key={`latest-${index}`} className="product-card">
             <img
-              src="./images/28.JPG"
+              // src="./images/28.JPG"
               alt={`상품 이미지 ${index + 1}`}
               className="product-image"
             />
