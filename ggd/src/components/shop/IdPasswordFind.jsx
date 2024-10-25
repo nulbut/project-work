d470420b96d4dcf3a2ae92925ed17ae27d135127
your_name <your_email>
2024-10-25 10:15:30 +0900

    modified:   ggd/src/components/shop/IdPasswordFind.jsx

--- ggd/src/components/shop/IdPasswordFind.jsx
+++ ggd/src/components/shop/IdPasswordFind.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
+let mailck = false; // 이메일 인증 확인 여부
+
 const IdPasswordFind = ({ sucFind }) => {
   const nav = useNavigate();
 
@@ -43,56 +45,68 @@ const IdPasswordFind = ({ sucFind }) => {
   const emailmatch = () => {
     if (code === userCode) {
       alert("인증번호 일치 합니다.");
+      mailck = "ok";
     } else {
       alert("인증번호 다시 확인해주세요.");
+      mailck = "fail";
     }
+  };
+
+  const idfund = (form) => {
+    console.log(form);
   };
 
   return (
     <div>
-      {/*아이디,비밀번호 찾기*/}
-      <h1>아이디, 비밀번호 찾기</h1>
+      <form className="content" onSubmit={handleSubmit}>
+        {/*아이디,비밀번호 찾기*/}
+        <h1>아이디, 비밀번호 찾기</h1>
 
-      <p>가입하셨던 성함과 이메일을 기입 해주십시오.</p>
-      <div className="contetnt">
-        <div className="name">
-          <p>이름</p>
-          <input
-            placeholder="이름,대표자 이름"
-            {...register("name", {
-              required: {
-                value: true,
-                message: "이름 or 대표자 이름 입력해주세요.",
-              },
-            })}
-          />
+        <p>가입하셨던 성함과 이메일을 기입 해주십시오.</p>
+        <div className="contetnt">
+          <div className="name">
+            <p>이름</p>
+            <input
+              placeholder="이름,대표자 이름"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "이름 or 대표자 이름 입력해주세요.",
+                },
+              })}
+            />
+          </div>
+          <div className="email">
+            <p>E-mail</p>
+            <input
+              placeholder="you@example.com"
+              {...register("nemail", {
+                required: {
+                  value: true,
+                  message: "이메일 주소는 필수 입력값입니다.",
+                },
+                pattern: {
+                  value:
+                    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i,
+                  message: "'you@example.com' 형식으로 입력 해주세요.",
+                },
+              })}
+            />
+            <input
+              placeholder="인증번호 입력"
+              onChange={onch}
+              value={userCode}
+            />
+          </div>
+          <div className="button">
+            <Button onClick={mailCh}>인증 메일 보내기</Button>
+            <Button onClick={emailmatch}>인증번호 확인</Button>
+          </div>
+          <div>
+            <Button>아이디 찾기</Button>
+          </div>
         </div>
-        <div className="email">
-          <p>E-mail</p>
-          <input
-            placeholder="you@example.com"
-            {...register("nemail", {
-              required: {
-                value: true,
-                message: "이메일 주소는 필수 입력값입니다.",
-              },
-              pattern: {
-                value:
-                  /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i,
-                message: "'you@example.com' 형식으로 입력 해주세요.",
-              },
-            })}
-          />
-          <input placeholder="인증번호 입력" onChange={onch} value={userCode} />
-        </div>
-        <div className="button">
-          <Button onClick={mailCh}>인증 메일 보내기</Button>
-          <Button onClick={emailmatch}>인증번호 확인</Button>
-        </div>
-        <div>
-          <Button>아이디 찾기</Button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
