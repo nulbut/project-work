7f3250c1b3efb2993dc34a990c4a59bef095104f
kkr629 <kkr00629@gmail.com>
2024-10-28 01:14:41 +0900

    modified:   ggd/src/components/admin/AdminEx.jsx

--- ggd/src/components/admin/AdminEx.jsx
+++ ggd/src/components/admin/AdminEx.jsx
@@ -460,303 +460,35 @@ const AdminEx = ({ data }) => {
             >
               <div class="sb-sidenav-menu">
                 {buttons.map((el) => {
-                  const first = el.content;
-                  const second = el.depth;
-                  console.log(first);
                   return (
                     <div class="nav">
-                      <div class="sb-sidenav-menu-heading"></div>
-                      <a class="nav-link" href="index.html">
-                        <div class="sb-nav-link-icon">
-                          <i class="fas fa-tachometer-alt"></i>
-                        </div>
-                        Dashboard
-                      </a>
-                      <div class="sb-sidenav-menu-heading">상품 관리</div>
-                      <a
-                        class="nav-link collapsed"
-                        href="#"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapseLayouts"
-                        aria-expanded="false"
-                        aria-controls="collapseLayouts"
-                      >
-                        <div class="sb-nav-link-icon">
-                          <i class="fas fa-columns"></i>
-                        </div>
-                        Layouts
-                        <div class="sb-sidenav-collapse-arrow">
-                          <i class="fas fa-angle-down"></i>
-                        </div>
-                      </a>
-                      <div
-                        class="collapse"
-                        id="collapseLayouts"
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#sidenavAccordion"
-                      >
-                        <nav class="sb-sidenav-menu-nested nav">
-                          <a class="nav-link" href="layout-static.html">
-                            Static Navigation
-                          </a>
-                          <a class="nav-link" href="layout-sidenav-light.html">
-                            Light Sidenav
-                          </a>
-                        </nav>
-                      </div>
-                      <a
-                        class="nav-link collapsed"
-                        href="#"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapsePages"
-                        aria-expanded="false"
-                        aria-controls="collapsePages"
-                      >
-                        <div class="sb-nav-link-icon">
-                          <i class="fas fa-book-open"></i>
-                        </div>
-                        Pages
-                        <div class="sb-sidenav-collapse-arrow">
-                          <i class="fas fa-angle-down"></i>
-                        </div>
-                      </a>
-                      <div
-                        class="collapse"
-                        id="collapsePages"
-                        aria-labelledby="headingTwo"
-                        data-bs-parent="#sidenavAccordion"
-                      >
-                        <nav
-                          class="sb-sidenav-menu-nested nav accordion"
-                          id="sidenavAccordionPages"
-                        >
+                      <div class="sb-sidenav-menu-heading">{el.depth}</div>
+                      {el.content.map((el) => {
+                        console.log(el.detail);
+                        return (
                           <a
                             class="nav-link collapsed"
                             href="#"
                             data-bs-toggle="collapse"
-                            data-bs-target="#pagesCollapseAuth"
+                            data-bs-target="#collapsePages"
                             aria-expanded="false"
-                            aria-controls="pagesCollapseAuth"
+                            aria-controls="collapsePages"
                           >
-                            Authentication
+                            <div class="sb-nav-link-icon">
+                              <i class="fas fa-book-open"></i>
+                            </div>
+                            {el.name}
                             <div class="sb-sidenav-collapse-arrow">
                               <i class="fas fa-angle-down"></i>
                             </div>
                           </a>
-                          <div
-                            class="collapse"
-                            id="pagesCollapseAuth"
-                            aria-labelledby="headingOne"
-                            data-bs-parent="#sidenavAccordionPages"
-                          >
-                            <nav class="sb-sidenav-menu-nested nav">
-                              <a class="nav-link" href="login.html">
-                                Login
-                              </a>
-                              <a class="nav-link" href="register.html">
-                                Register
-                              </a>
-                              <a class="nav-link" href="password.html">
-                                Forgot Password
-                              </a>
-                            </nav>
-                          </div>
-                          <a
-                            class="nav-link collapsed"
-                            href="#"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#pagesCollapseError"
-                            aria-expanded="false"
-                            aria-controls="pagesCollapseError"
-                          >
-                            Error
-                            <div class="sb-sidenav-collapse-arrow">
-                              <i class="fas fa-angle-down"></i>
-                            </div>
-                          </a>
-                          <div
-                            class="collapse"
-                            id="pagesCollapseError"
-                            aria-labelledby="headingOne"
-                            data-bs-parent="#sidenavAccordionPages"
-                          >
-                            <nav class="sb-sidenav-menu-nested nav">
-                              <a class="nav-link" href="401.html">
-                                401 Page
-                              </a>
-                              <a class="nav-link" href="404.html">
-                                404 Page
-                              </a>
-                              <a class="nav-link" href="500.html">
-                                500 Page
-                              </a>
-                            </nav>
-                          </div>
-                        </nav>
-                      </div>
-                      <div class="sb-sidenav-menu-heading">Addons</div>
-                      <a class="nav-link" href="charts.html">
-                        <div class="sb-nav-link-icon">
-                          <i class="fas fa-chart-area"></i>
-                        </div>
-                        Charts
-                      </a>
-                      <a class="nav-link" href="tables.html">
-                        <div class="sb-nav-link-icon">
-                          <i class="fas fa-table"></i>
-                        </div>
-                        Tables
-                      </a>
+                        );
+                      })}
                     </div>
                   );
                 })}
               </div>
-              <div class="sb-sidenav-menu">
-                <div class="nav">
-                  <div class="sb-sidenav-menu-heading">회원 관리</div>
-                  <a class="nav-link" href="index.html">
-                    <div class="sb-nav-link-icon">
-                      <i class="fas fa-tachometer-alt"></i>
-                    </div>
-                    Dashboard
-                  </a>
-                  <div class="sb-sidenav-menu-heading">상품 관리</div>
-                  <a
-                    class="nav-link collapsed"
-                    href="#"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseLayouts"
-                    aria-expanded="false"
-                    aria-controls="collapseLayouts"
-                  >
-                    <div class="sb-nav-link-icon">
-                      <i class="fas fa-columns"></i>
-                    </div>
-                    Layouts
-                    <div class="sb-sidenav-collapse-arrow">
-                      <i class="fas fa-angle-down"></i>
-                    </div>
-                  </a>
-                  <div
-                    class="collapse"
-                    id="collapseLayouts"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#sidenavAccordion"
-                  >
-                    <nav class="sb-sidenav-menu-nested nav">
-                      <a class="nav-link" href="layout-static.html">
-                        Static Navigation
-                      </a>
-                      <a class="nav-link" href="layout-sidenav-light.html">
-                        Light Sidenav
-                      </a>
-                    </nav>
-                  </div>
-                  <a
-                    class="nav-link collapsed"
-                    href="#"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapsePages"
-                    aria-expanded="false"
-                    aria-controls="collapsePages"
-                  >
-                    <div class="sb-nav-link-icon">
-                      <i class="fas fa-book-open"></i>
-                    </div>
-                    Pages
-                    <div class="sb-sidenav-collapse-arrow">
-                      <i class="fas fa-angle-down"></i>
-                    </div>
-                  </a>
-                  <div
-                    class="collapse"
-                    id="collapsePages"
-                    aria-labelledby="headingTwo"
-                    data-bs-parent="#sidenavAccordion"
-                  >
-                    <nav
-                      class="sb-sidenav-menu-nested nav accordion"
-                      id="sidenavAccordionPages"
-                    >
-                      <a
-                        class="nav-link collapsed"
-                        href="#"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#pagesCollapseAuth"
-                        aria-expanded="false"
-                        aria-controls="pagesCollapseAuth"
-                      >
-                        Authentication
-                        <div class="sb-sidenav-collapse-arrow">
-                          <i class="fas fa-angle-down"></i>
-                        </div>
-                      </a>
-                      <div
-                        class="collapse"
-                        id="pagesCollapseAuth"
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#sidenavAccordionPages"
-                      >
-                        <nav class="sb-sidenav-menu-nested nav">
-                          <a class="nav-link" href="login.html">
-                            Login
-                          </a>
-                          <a class="nav-link" href="register.html">
-                            Register
-                          </a>
-                          <a class="nav-link" href="password.html">
-                            Forgot Password
-                          </a>
-                        </nav>
-                      </div>
-                      <a
-                        class="nav-link collapsed"
-                        href="#"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#pagesCollapseError"
-                        aria-expanded="false"
-                        aria-controls="pagesCollapseError"
-                      >
-                        Error
-                        <div class="sb-sidenav-collapse-arrow">
-                          <i class="fas fa-angle-down"></i>
-                        </div>
-                      </a>
-                      <div
-                        class="collapse"
-                        id="pagesCollapseError"
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#sidenavAccordionPages"
-                      >
-                        <nav class="sb-sidenav-menu-nested nav">
-                          <a class="nav-link" href="401.html">
-                            401 Page
-                          </a>
-                          <a class="nav-link" href="404.html">
-                            404 Page
-                          </a>
-                          <a class="nav-link" href="500.html">
-                            500 Page
-                          </a>
-                        </nav>
-                      </div>
-                    </nav>
-                  </div>
-                  <div class="sb-sidenav-menu-heading">Addons</div>
-                  <a class="nav-link" href="charts.html">
-                    <div class="sb-nav-link-icon">
-                      <i class="fas fa-chart-area"></i>
-                    </div>
-                    Charts
-                  </a>
-                  <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon">
-                      <i class="fas fa-table"></i>
-                    </div>
-                    Tables
-                  </a>
-                </div>
-              </div>
+
               <div class="sb-sidenav-footer">
                 <div class="small">Logged in as:</div>
                 Start Bootstrap
@@ -832,10 +564,7 @@ const AdminEx = ({ data }) => {
                         Area Chart Example
                       </div>
                       <div class="card-body">
-                        <div className>
-                          <MyResponsivePie data={dataPie} />
-                          123
-                        </div>
+                        <MyResponsivePie data={dataPie} />
                       </div>
                     </div>
                   </div>
@@ -843,15 +572,10 @@ const AdminEx = ({ data }) => {
                     <div class="card mb-4">
                       <div class="card-header">
                         <i class="fas fa-chart-bar me-1"></i>
-                        <MyResponsiveLine data={dataLine} />
                         Bar Chart Example
                       </div>
                       <div class="card-body">
-                        <canvas
-                          id="myBarChart"
-                          width="100%"
-                          height="40"
-                        ></canvas>
+                        <MyResponsiveLine data={dataLine} />
                       </div>
                     </div>
                   </div>
