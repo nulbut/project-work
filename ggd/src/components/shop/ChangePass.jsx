45f3bb16dcc2ce8822796e5f203bad8e45fdf160
your_name <your_email>
2024-10-28 08:50:11 +0900

    modified:   ggd/src/components/shop/ChangePass.jsx

--- ggd/src/components/shop/ChangePass.jsx
+++ ggd/src/components/shop/ChangePass.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ChangePass = () => {
   //비밀번호 변경
   const nav = useNavigate();
 
+  const { state } = useLocation();
+  const { fid } = state;
+  console.log(fid);
   return (
     <div>
       <h1>비밀번호 변경 화면</h1>
