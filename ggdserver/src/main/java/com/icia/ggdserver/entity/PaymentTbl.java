81e2122dbe3f5e16d5fc70614652fe471a6a39d6
donghoon99 <donghoon900@gmail.com>
2024-11-11 17:20:41 +0900

    new file:   ggdserver/src/main/java/com/icia/ggdserver/controller/CustomerController.java 	new file:   ggdserver/src/main/java/com/icia/ggdserver/entity/CustomerTbl.java 	modified:   ggdserver/src/main/java/com/icia/ggdserver/entity/PaymentTbl.java 	new file:   ggdserver/src/main/java/com/icia/ggdserver/repository/CustomerRepository.java 	new file:   ggdserver/src/main/java/com/icia/ggdserver/service/CustomerService.java
    Changes not staged for commit:
    	modified:   ggd/src/App.jsx
    	modified:   ggd/src/components/shop/ProductDetails.jsx
    	modified:   ggd/src/components/toss/vite.config.ts
    	modified:   ggd/src/components/toss/widget/WidgetCheckout.jsx
    	modified:   ggd/src/components/toss/widget/WidgetSuccess.jsx
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/controller/CustomerController.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/controller/PaymentController.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/entity/CustomerTbl.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/entity/PaymentTbl.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/repository/CustomerRepository.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/repository/PaymentRepository.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/service/CustomerService.java
    	modified:   ggdserver/src/main/java/com/icia/ggdserver/service/PaymentService.java
    	modified:   ggdserver/src/main/resources/application.properties
    
    Untracked files:
    	ggdserver/src/main/webapp/usupload/1731293194811.JPG
    	ggdserver/src/main/webapp/usupload/1731293458573.jpg
    	ggdserver/src/main/webapp/usupload/1731293494054.jpg

--- ggdserver/src/main/java/com/icia/ggdserver/entity/PaymentTbl.java
+++ ggdserver/src/main/java/com/icia/ggdserver/entity/PaymentTbl.java
@@ -1,24 +1,2 @@
-package com.icia.ggdserver.entity;
-
-import java.time.LocalDateTime;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "payment_tbl")
-@Data
-public class PaymentTbl {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long payid;
-
-    private String orderId;
-
-    private String amount;
-
-    private String paymentKey;
-
-    private LocalDateTime paymentDate;
+package com.icia.ggdserver.entity;public class PaymentTbl {
 }
-
